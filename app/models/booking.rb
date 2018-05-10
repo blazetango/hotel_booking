ee73bb27496ca1cff4a493ca98ef9077727e9006
chetan <blazetango@gmail.com>
2018-05-10 21:14:34 +0530

    indented , room drop down removed from form

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,15 +1,14 @@
 class Booking < ApplicationRecord
-	belongs_to :room
+  #belongs_to :room
 
   validates :from_date, inclusion: { in: (Date.today..Date.today+6.months) }
-
   before_create :check_customer_registered
 
   private
   def check_customer_registered
-  	customer = User.find_by_email(self.customer_email)
-  	if customer.present?
-  		self.customer_id = customer.id
-  	end
+    customer = User.find_by_email(self.customer_email)
+    if customer.present?
+      self.customer_id = customer.id
+    end
   end
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,7 +2,6 @@ class Customer < User
 
   after_create :update_booking_with_registered_customer
 
-
   private
   def update_booking_with_registered_customer
     bookings = Booking.where(customer_email: self.email).where(customer_id: nil)
